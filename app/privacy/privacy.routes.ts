d2da24d7182102dd7d2ca1acf9d5d6ea533bb304
Jack24832384 <2123765049@qq.com>
2026-04-26 12:35:25 +0800

    Delete app directory

--- app/privacy/privacy.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { PrivacyPage } from './privacy.page';
-
-// Routing for Privacy page
-export const privacyRoutes: Routes = [
-  { path: '', component: PrivacyPage }
-];
